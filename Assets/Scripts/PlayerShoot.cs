8c25658ffde2d24f8cc5c387dd24d387f4340722
gergo247 <gergo247@gmail.com>
2020-06-02 00:24:35 +0200

    Weapon class, Firerate
    Adds Class for weapons and fire rate function

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using Mirror;
 
+[RequireComponent(typeof(WeaponManager))]
 public class PlayerShoot : NetworkBehaviour
 {
     private const string PLAYER_TAG = "Player";
@@ -10,12 +11,9 @@ public class PlayerShoot : NetworkBehaviour
     private Camera camera;
     [SerializeField]
     private LayerMask mask;
-    [SerializeField]
-    private PlayerWeapon weapon;
-    [SerializeField]
-    private GameObject weaponGFX;
-    [SerializeField]
-    private string weaponLayerName = "Weapon";
+   
+    private PlayerWeapon currentWeapon;
+    private WeaponManager weaponManager;
 
     void Start()
     {
@@ -24,27 +22,47 @@ public class PlayerShoot : NetworkBehaviour
             Debug.Log("PlayerShoot : No camera referenced");
             this.enabled = false;
         }
-        weaponGFX.layer = LayerMask.NameToLayer(weaponLayerName);
+
+        weaponManager = GetComponent<WeaponManager>();
     }
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        currentWeapon = weaponManager.GetCurrentWeapon();
+        //single frie
+        if (currentWeapon.fireRate <= 0)
         {
-            Shoot();
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
         }
+        else
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                InvokeRepeating("Shoot",0f,1f/currentWeapon.fireRate);
+            }
+            else if(Input.GetButtonUp("Fire1"))
+            {
+                CancelInvoke("Shoot");
+            }
+        }
+       
     }
     //client - called only on client
     [Client]
     void Shoot()
     {
+        Debug.Log("Shoot!");
+
         RaycastHit _hit;
-        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out _hit, weapon.range, mask))
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out _hit, currentWeapon.range, mask))
         {
             //we hit something
             Debug.Log("We hit" + _hit.collider.name);
             if (_hit.collider.tag == PLAYER_TAG)
             {
-                CmdPlayerShot(_hit.collider.name, weapon.damage);
+                CmdPlayerShot(_hit.collider.name, currentWeapon.damage);
             }
         }
     }
--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Mirror;
 
-public class WeaponManager : MonoBehaviour
+public class WeaponManager : NetworkBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Transform weaponHolder;
+    [SerializeField]
+    private string weaponLayerName = "Weapon";
+
+    [SerializeField]
+    private PlayerWeapon primaryWeapon;
+
+    private PlayerWeapon currentWeapon;
+
     void Start()
     {
-        
+        EquipWeapon(primaryWeapon);
+
+       // weaponGFX.layer = LayerMask.NameToLayer(weaponLayerName);
+
+    }
+    public PlayerWeapon GetCurrentWeapon()
+    {
+        return currentWeapon;
     }
 
-    // Update is called once per frame
-    void Update()
+    void EquipWeapon(PlayerWeapon _weapon)
     {
-        
-    }
+        currentWeapon = _weapon;
+        GameObject _weaponIns =  (GameObject)Instantiate(_weapon.graphics, weaponHolder.position, weaponHolder.rotation);
+        //set parent so it follows movement
+        _weaponIns.transform.SetParent(weaponHolder);
+        if (isLocalPlayer)
+            _weaponIns.layer = LayerMask.NameToLayer(weaponLayerName);
+
+    } 
 }
